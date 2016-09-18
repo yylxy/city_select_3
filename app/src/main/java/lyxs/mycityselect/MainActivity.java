0c9c34df042966f624dace4f42764afe77061452
阳 <lyxs916@163.com>
2016-09-18 23:40:06 +0800

    88

---
+++ app/src/main/java/lyxs/mycityselect/MainActivity.java
@@ -0,0 +1,31 @@
+package lyxs.mycityselect;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import lyxs.city.CitySelectActivity;
+
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView select = (TextView) findViewById(R.id.select);
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, CitySelectActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+}
---
+++ city/src/main/java/lyxs/city/CitySelectActivity.java
@@ -0,0 +1,17 @@
+package lyxs.city;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CitySelectActivity extends Activity  {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_select);
+
+    }
+
+
+}
